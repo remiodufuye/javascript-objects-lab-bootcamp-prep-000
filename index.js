5ed23adafe299f54f43c47bb12e9a07145501c34
remi.odufuye <remi.odufuye@gmail.com>
2019-10-02 20:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var recipes = {};
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
     {
-      
+         object[key] = value ;
+         return object ; 
     }
     
 function deleteFromObjectByKey(object, key) {
