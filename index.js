3e9662fb7a3f6a0b80624374ed62de2bc541e4f1
remi.odufuye <remi.odufuye@gmail.com>
2019-10-02 20:06:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var recipes = {};
 function deleteFromObjectByKey(object, key) {
   
     var newObj = Object.assign({} , object ) ; 
-    delete.newObj.key ;
+    delete newObj.key ;
     return newObj ; 
     
 }
